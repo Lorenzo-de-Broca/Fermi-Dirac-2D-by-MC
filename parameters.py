511b745084fb5ec6719f7eb8de0f36c69e50a4a8
raphael.ros <raphael.ros@student-cs.fr>
2025-10-29 11:46:03 +0100

    meilleure précision des grandeurs physiques

--- parameters.py
+++ parameters.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # constantes physiques
-h = 6.67e-34 #J.s
+h = 6,62607015e-34 #J.s
 hbar = h/(2*np.pi) #J.s.rad^(-1)
-k_b = 1.38e-23 #J.K^(-1)
-m_e = 9.11e-31 #kg (masse de l'électron)
+k_b = 1,380649e-23 #J.K^(-1)
+m_e = 9,109e-31 #kg (masse de l'électron)
